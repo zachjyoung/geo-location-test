e8f411ccb694788106a6319f333aee795d897499
Zach Young <zachjyoung@gmail.com>
2013-06-20 11:31:34 -0400

    initial build

---
+++ geo_location/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoLocation::Application.config.secret_token = '9a3c9369c924842954c5ec9202be3a0677a051740ea5f68fd96f42c637c771585158e9dbd0ba8d6dd520b549aa06b963c1464412337f943fd33e54240a6e0fca'
